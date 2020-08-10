eb16e86396558802bcef38232308005d90dc5908
alan's MacBook Pro <251956250@qq.com>
2020-08-10 17:42:51 +0800

    first

---
+++ README.md
@@ -0,0 +1,39 @@
+## AliCloudTSDB
+阿里云时序数据库HTTP请求API接口封装,因为公司项目着急使用，暂时先以能用为主，后期会不断完善更新
+
+## 安装方法
+```
+composer require coolelephant/aliyuntsdbapi
+```
+## 使用方法
+本方法支持命名空间，如您的项目支持自动加载，直接实例化即可
+```
+$huaweiCloud = new \CoolElephant\AliCloudTSDB\AliCloudTSDB('a1d1f50cad21415fbdd13d8f53d36d60','cfc881cc704c4fba8d8fef5788e03e6b');
+$response = $huaweiCloud->option(true)
+                        ->uri('/rest/caas/relationnumber/partners/v1.0')
+                        ->method('POST')
+                        ->data(['relationNum' => 1888888888,'callerNum' => 18666666666])
+                        ->request();
+```
+
+如您的项目不支持自动加载，则需要再上面的基础上引入
+```
+require_once('../vendor/autoload.php');
+```
+
+
+返回结果已做处理，以array 数组的形式输出
+```
+[
+    'resultcode'        =>  0,
+    'resultdesc'        =>  'Success',
+    'subscriptionId'    =>  'f36934ad-74b3-4666-85bc-05f0fbcb46f5',
+    'relationNum'       =>  '+8616558940111',
+    'callDirection'     =>  0,
+    'duration'          =>  120,
+    'maxDuration'       =>  1
+];
+```
+
+## 具体返回值请查看官方接口
+[点击查看官方文档](https://help.aliyun.com/document_detail/63557.html)
---
+++ src/AliCloudTSDB.php
@@ -0,0 +1,116 @@
+<?php
+// +----------------------------------------------------------------------
+// | CoolCms [ DEVELOPMENT IS SO SIMPLE ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2018-2019 http://www.coolcms.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +---------------------------------------------------------------------
+// | Author: Alan <251956250@qq.com>
+// +----------------------------------------------------------------------
+// | DateTime: 2020/8/10 14:54
+// +----------------------------------------------------------------------
+// | Desc: 
+// +----------------------------------------------------------------------
+
+namespace CoolElephant\AliCloudTSDB;
+
+
+/**
+ * 封装阿里云时序数据库TSDB的HTTP请求
+ * 说明文档：https://help.aliyun.com/document_detail/63557.html?spm=a2c4g.11186623.6.646.26e41a05avR0vh
+ * Class AliCloudTSDB
+ * @package CoolElephant\AliCloud
+ */
+class AliCloudTSDB
+{
+
+    /**
+     * TSDB 实例访问链接
+     * @var null
+     */
+    private $hosts = null;
+    /**
+     * 请求方法，包含GET,POST,PUT,DELETE
+     * @var string
+     */
+    private $method = 'GET';
+    /**
+     * TSDB API 支持通过添加查询字符串参数 method_override 来替代 HTTP methods/verbs
+     * 使用方法，作为参数直接放到url后面
+     * 包含：method_override=post,method_override=put,method_override=delete,其中get默认不用上传
+     * @var null
+     */
+    private $method_override = null;
+    /**
+     * 具体的API接口url
+     * @var null
+     */
+    private $api = null;
+    /**
+     * 具体请求参数
+     * @var null
+     */
+    private $param = null;
+
+    public function __construct($hosts=null,$method=null,$api=null,$param=null)
+    {
+        if(empty($hosts) || empty($method) || empty($api)){
+            return ['resultcode'=>4001,'resultdesc'=>'必传参数不能为空，请检查hosts，method或api参数是否为空'];
+            die;
+        }
+        $this->hosts = $hosts;
+        $this->method = $method;
+        $this->method_override = $this->switchToMethod($method);
+        $this->api = $api;
+        $this->param = $param;
+    }
+
+    public function request(){
+        $url = $this->hosts.$this->api.'?'.$this->method_override.http_build_query($this->param);
+        try{
+            $response = file_get_contents($url,false,stream_context_create($this->header()));
+            return $response;
+        }catch (\Exception $exception){
+            return $exception;
+        }
+    }
+
+    private function header(){
+
+        return [
+            'http' => [
+                'method' => $this->method, // 请求方法
+                'content'   =>  json_encode($this->param),
+                'header' => [
+                    'Accept: application/json',
+                    'Content-Type: application/json;charset=UTF-8',
+                ],  //请求头部
+                'ignore_errors' => true // 返回json格式
+            ]
+        ];
+    }
+    /**
+     * method转换成功method_override
+     * @param $method
+     * @return string
+     */
+    private function switchToMethod($method){
+        $result = '';
+        switch (strtolower($method)){
+            case 'post':
+                $result = 'method_override=post';
+                break;
+            case 'put':
+                $result = 'method_override=put';
+                break;
+            case 'delete':
+                $result = 'method_override=delete';
+                break;
+            default:
+                $result = 'method_override=get';
+                break;
+        }
+        return $result.'&';
+    }
+}
